9cfc940b985fdcf705a2fcc60d84c993bd09a545
Noel Beristain <noelberistain@gmail.com>
2024-09-29 20:37:28 -0700

    update App file * implement components * handle login * handle error * handle login button disabled * integrate logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { ErrorResponse, useLoginMutation } from './features/api/apiSlice';
+import { Button, Input } from './components';
+
+import openSendLogo from './assets/opensend-logo.svg';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const [error, setError] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [login] = useLoginMutation();
+
+	const handleLogin = async () => {
+		try {
+			const result = await login({ email, password }).unwrap();
+			if (error) {
+				setError('');
+			}
+			console.log('🚀 ~ handleLogin ~ response:', {
+				result,
+			});
+		} catch (error) {
+			console.log('CATCH');
+			console.log(error);
+			setError((error as ErrorResponse).data.message);
+		}
+	};
+	return (
+		<section
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '1rem',
+				alignItems: 'center',
+				width: '400px',
+				paddingTop: '1rem',
+			}}>
+			<div className='logo'>
+				<img src={openSendLogo} alt='opensend logo' width={16} />
+				<span>opensend</span>
+			</div>
+			<div className='card'>
+				<p>Welcome back!</p>
+				<p>Log in to continue with Opensend</p>
+
+				<Input
+					placeholder='Enter email'
+					type='email'
+					value={email}
+					handleOnChange={setEmail}
+				/>
+
+				<Input
+					placeholder='Enter password'
+					type='password'
+					value={password}
+					handleOnChange={setPassword}
+				/>
+
+				{error && <p className='error-message'>{error}</p>}
+
+				<Button label='Forgot Your Password' onClick={() => {}} />
+				<Button
+					label='Log in'
+					disabled={!email || !password}
+					onClick={handleLogin}
+				/>
+			</div>
+		</section>
+	);
 }
 
-export default App
+export default App;
